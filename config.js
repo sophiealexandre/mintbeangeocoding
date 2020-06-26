4e925034dc572399447deaf1bb2bd4516b54a68e
Sophie Alexandre <55605315+sophiealexandre@users.noreply.github.com>
2020-06-26 10:40:07 -0400

    Delete config.js

--- config.js
+++
@@ -1,2 +0,0 @@
-const ACCESS_TOKEN = 'pk.eyJ1Ijoic29waGllYWxleGFuZHJlIiwiYSI6ImNrYmh6dDh4NzBhaDkycm1seWdjeWk1d2UifQ.MEBMcxLbttFKOuZBW9vTfQ';
-const MAPBOX_STYLE = 'mapbox://styles/mapbox/streets-v11';
