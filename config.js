461d18346f8d48edc146d0e334a282b08e5e2126
Sophie Alexandre <55605315+sophiealexandre@users.noreply.github.com>
2020-06-26 10:39:22 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,2 @@
+const ACCESS_TOKEN = 'pk.eyJ1Ijoic29waGllYWxleGFuZHJlIiwiYSI6ImNrYmh6dDh4NzBhaDkycm1seWdjeWk1d2UifQ.MEBMcxLbttFKOuZBW9vTfQ';
+const MAPBOX_STYLE = 'mapbox://styles/mapbox/streets-v11';
